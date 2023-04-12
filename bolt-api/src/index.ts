704d7747b4997bd9bbb6a63c36e67ca80e44924c
raobystorm <raobystorm@gmail.com>
2023-04-13 01:30:44 +0900

    added api handler

--- bolt-api/src/index.ts
+++ bolt-api/src/index.ts
@@ -2,9 +2,18 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
+app.get("/timeline/:userId", async (req, res) => {
+    let page = req.query.page;
+    let lang = req.query.lang;
+
+    res.send("Time line");
 });
+
+app.get("/articles/:articleId",async (req, res) => {
+    let lang = req.query.lang;
+
+    res.send("article page");
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
