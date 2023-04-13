ba6d82695157f11eb09127823ee9549207bea8d6
Rui <raobystorm@gmail.com>
2023-04-13 23:44:24 +0900

    update

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS bolt CASCADE; -- drop all tables in schema.sql if exist
+DROP DATABASE IF EXISTS bolt; -- drop all tables in schema.sql if exist
 CREATE DATABASE bolt; -- create database bolt
 USE bolt; -- use database bolt
