5e9df6025d004dceda26b0eb7752d2aff3895b6a
Rui <raobystorm@gmail.com>
2023-04-14 00:29:26 +0900

    update

--- crawler/summarize/summerize.py
+++ crawler/summarize/summerize.py
@@ -7,6 +7,3 @@ from langchain.prompts import PromptTemplate
 os.environ["OPENAI_API_KEY"] = os.environ["OPENAI_API_KEY"]
 
 prompt = "This is a test"
-
-def main() -> None:
-    
--- crawler/translate/translate.py
+++ crawler/translate/translate.py
@@ -1,25 +1,27 @@
 import os
-
 import openai
 
 
-def get_translation(text: str, lang: str) -> str:
+def get_title_translation(title: str, lang: str) -> str:
     openai.api_key = os.environ["OPENAI_API_KEY"]
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
-                "content": f"You're a translator who translate news article into {lang}",
+                "content": f"You're a translator who translate news title into {lang}",
             },
-            {"role": "user", "content": f"Translate the news article below: {text}"},
+            {
+                "role": "user",
+                "content": f"Translate the news title into {lang} below: {title}",
+            },
             {
                 "role": "assistant",
                 "content": f"Translation result of {lang}: ",
             },
         ],
         temperature=0,
-        max_tokens=400,
+        max_tokens=1000,
     )
 
     return response["choices"][0]["message"]["content"]
