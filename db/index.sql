b12cfa9fba136c74195638cb1a6bcaf4e397660e
Rui <raobystorm@gmail.com>
2023-04-22 13:39:57 +0000

    added mysql db for dev container

--- db/index.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX user_media_index ON user_media (media_id, lang);
-CREATE INDEX user_article_index ON user_article (user_id);
--- db/schema.sql
+++ db/schema.sql
@@ -64,3 +64,6 @@ CREATE TABLE user_media (
   FOREIGN KEY (user_id) REFERENCES user(id),
   FOREIGN KEY (media_id) REFERENCES media(id)
 );
+
+CREATE INDEX user_media_index ON user_media (media_id, lang);
+CREATE INDEX user_article_index ON user_article (user_id);
